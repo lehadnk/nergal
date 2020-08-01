03c16ad3fd2dcdfe5a51957dbe10f1c6b86c066e
Alexey Zauzin <lehadnk@gmail.com>
2020-08-01 20:36:17 +0300

    Added service container

--- run.ts
+++ run.ts
@@ -1,25 +1,5 @@
 import "reflect-metadata";
-import {config as dotenvInit} from 'dotenv';
-import {DiscordService} from "./src/DiscordService";
-import {Database} from "sqlite3";
-import {SqliteDbAdapter} from "./src/SqliteDbAdapter";
-import {Client} from "discord.js";
+import ServiceContainer from "./src/ServiceContainer";
 
-dotenvInit();
-
-let databaseFile = './' + process.env.APP_ENV + '-db.db3';
-let db = new Database(databaseFile);
-let adapter = new SqliteDbAdapter(db);
-
-let admins = new Map<string, null>();
-let adminIds = JSON.parse(process.env.ADMIN_IDS);
-adminIds.forEach(e => admins.set(e, null));
-
-let discordClient = new Client();
-let service = new DiscordService(
-    discordClient,
-    process.env.DISCORD_BOT_TOKEN,
-    admins
-);
-
-service.start();
+ServiceContainer.init();
+ServiceContainer.start();
---
+++ src/ServiceContainer.ts
@@ -0,0 +1,38 @@
+import {IDbAdapter} from "./IDbAdapter";
+import {Database} from "sqlite3";
+import {Client} from 'discord.js';
+import {SqliteDbAdapter} from "./SqliteDbAdapter";
+import {DiscordService} from "./DiscordService";
+import {config as dotenvInit} from "dotenv";
+
+export default class ServiceContainer {
+    static db: IDbAdapter;
+    static discordClient: Client;
+    static discordService: DiscordService;
+
+    public static init()
+    {
+        dotenvInit();
+
+        this.discordClient = new Client();
+
+        let databaseFile = './' + process.env.APP_ENV + '-db.db3';
+        let db = new Database(databaseFile);
+        this.db = new SqliteDbAdapter(db);
+
+        let admins = new Map<string, null>();
+        let adminIds = JSON.parse(process.env.ADMIN_IDS);
+        adminIds.forEach(e => admins.set(e, null));
+
+        this.discordService = new DiscordService(
+            ServiceContainer.discordClient,
+            process.env.DISCORD_BOT_TOKEN,
+            admins
+        );
+    }
+
+    public static start()
+    {
+        this.discordService.start();
+    }
+}
