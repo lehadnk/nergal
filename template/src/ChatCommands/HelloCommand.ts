830beecb8a878e0962c39314607465be471667d4
Alexey Zauzin <lehadnk@gmail.com>
2021-04-11 01:11:50 -0500

    Fixed a bug with app skeleton

--- template/src/ChatCommands/HelloCommand.ts
+++ template/src/ChatCommands/HelloCommand.ts
@@ -6,7 +6,7 @@ import {IChatCommand} from "nergal";
 export default class HelloCommand extends AbstractChatCommand implements IChatCommand {
     command: string = '!hello';
 
-    public handle(message: DiscordMessage, args: string[]): DiscordControllerResponse
+    public async handle(message: DiscordMessage, args: string[]): Promise<DiscordControllerResponse>
     {
         return new DiscordControllerResponse("Hello, " + message.authorName + "!");
     }
--- template/src/ChatCommands/index.ts
+++ template/src/ChatCommands/index.ts
@@ -1,1 +1,1 @@
-export {default as ListGuilds} from './ListGuilds';
+export {default as HelloCommand} from './HelloCommand';
